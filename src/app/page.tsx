9082df11e17e37718e5a6d86ca94081b6f44a9db
Laia <laia.font2@uvic.cat>
2024-02-15 16:40:27 +0100

    Search bar design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import connect from "../app/services/api";
 export default function Home() {
   const [username, setUsername] = useState("");
   const [userExists, setUserExists] = useState(true);
-  const [userData, setUserData] = useState({ 'avatar_url': '', 'name': '', 'bio': '' });
+  const [userData, setUserData] = useState({'avatar_url': '', 'name': '', 'bio': ''});
 
   async function searchUser(value: string) {
     try {
@@ -23,7 +23,7 @@ export default function Home() {
         <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
           Enter a GitHub Username
         </label>
-        <div className="flex flex-row justify-between relative mt-2 rounded-md shadow-sm">
+        <div className="flex flex-row relative mt-2 rounded-md shadow-sm">
           <input
             type="text"
             name="username"
@@ -32,24 +32,39 @@ export default function Home() {
             placeholder="Search for a user..."
             onChange={(e) => setUsername(e.target.value)}
           />
-          <button className="block rounded-md border-0 mx-1 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6" onClick={() => searchUser(username)}>Search</button>
+          <button className="block rounded-md border-0 mx-1 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 focus:bg-gray-200" onClick={() => searchUser(username)}>Search</button>
         </div>
       </div>
+      <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
       <div>
         {userExists ? (
-          <div className="columns-2">
-            <div>
+          <div className="grid grid-cols-3">
+            <div className="flex flex-col">
               <div>
                 <img src="https://avatars.githubusercontent.com/u/74786722?v=4" alt="User avatar" className="rounded-full ring-1 ring-inset ring-gray-300" />
               </div>
+              <div>
+                <p className="text-2xl font-bold">Laia</p>
+                <p className="text-lg">AAAAAAAAAAAAAAAAAAA</p>
+              </div>
             </div>
-            <div>
-              <p className="text-2xl font-bold">{userData.name}</p>
-              <p className="text-lg">{userData.bio}</p>
+            <div className="col-span-2">
+              <div className="flex flex-row relative mt-2 rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  placeholder="Find a repository..."
+                />
+                <button className="block rounded-md border-0 mx-1 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 focus:bg-gray-200" onClick={() => searchUser(username)}>Type</button>
+                <button className="block rounded-md border-0 mx-1 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 focus:bg-gray-200" onClick={() => searchUser(username)}>Language</button>
+              </div>
+              <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
             </div>
           </div>
         ) : (
-            <p className="m-2 text-rose-700 text-center">The user { username } could not be found.</p>
+          <p className="m-2 text-rose-700 text-center">The user {username} could not be found.</p>
         )}
       </div>
     </main>
