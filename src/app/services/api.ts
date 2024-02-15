6ea632f62a4229ab922e36e9f29d9ef3a269c204
Laia <laia.font2@uvic.cat>
2024-02-15 16:04:07 +0100

    Search bar with api call and initial design

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,9 +1,15 @@
 
-function connect(): void {
-    fetch("https://api.github.com/users/LaiaFont")
-    .then((res) => res.json())
-    .then((result) => { console.log(result) },
-      (error) => { console.log(error); })
+async function connect(user: string): Promise<{success: boolean, payload: any}>{
+    return fetch("https://api.github.com/users/" + user)
+        .then((res) => res.json())
+        .then(
+            (result) => {
+                return { 'success': true, 'payload': result };
+            },
+            (error) => {
+                return { 'success': false, 'payload': error };
+            }
+        );
 }
 
 export default connect;
