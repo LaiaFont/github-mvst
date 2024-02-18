158a136111410315307cbeb449c1206ea436b6c0
Laia <laia.font2@uvic.cat>
2024-02-18 20:43:09 +0100

    Add enter keydown on search bar

--- src/app/components/search-bar.tsx
+++ src/app/components/search-bar.tsx
@@ -8,9 +8,14 @@ export const SearchBar = ({ setUsername, searchUser, username }: { setUsername: 
                 type="text"
                 name="username"
                 id="username"
-                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6"
                 placeholder="Search for a user..."
                 onChange={(e) => setUsername(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        searchUser(username);
+                    }
+                }}
             />
             <button className="block rounded-md border-0 mx-1 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 focus:bg-gray-200" onClick={() => searchUser(username)}>Search</button>
         </div>
